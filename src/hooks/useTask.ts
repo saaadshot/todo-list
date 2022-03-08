0beebab18e427e3e04b86398beacb0e9c966b2b6
Saad <mohammed.saadullah@314ecorp.com>
2022-03-08 13:10:54 +0530

    Removed console.log

--- src/hooks/useTask.ts
+++ src/hooks/useTask.ts
@@ -3,7 +3,6 @@ import { getLocalStorage } from "./utils";
 
 const useTask: any = () => {
 	return useQuery<Task[]>(["tasks"], async () => {
-		console.log("useTask");
 		return getLocalStorage();
 	});
 };
